dddd45381394c7f9c2155cc1c2c612f413db4fb9
sailendrapavan <pavansailendra04@gmail.com>
2026-08-01 19:56:35 +0530

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+       return recur(nums,0);
+    }
+
+    public boolean recur(int[] nums, int index){
+        if(index>=nums.length-1){
+              return true;
+        }
+
+        boolean found = false;
+        for(int i=1;i<=nums[index];i++){
+            if(recur(nums,i+index)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean canJumpVersion2(int nums[]){
+     boolean[] visited = new boolean[nums.length];
+    visited[0] = true;
+        for (int i = 0; i < nums.length; i++) {
+            if (!visited[i] && i != 0)
+            continue;
+            int j = nums[i];
+            while (j > 0) {
+                if ((i + j < nums.length) && !(visited[i + j])) {
+                    visited[i + j] = true;
+                }
+                j--;
+            }
+        }
+        return visited[nums.length - 1];
+    }
+
+
+}
